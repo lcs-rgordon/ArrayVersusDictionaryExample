fcb3e620c8e925e906d63ff447c75e1040939381
Russell Gordon <rgordon@lcs.on.ca>
2025-05-21 10:58:55 -0400

    Initial Commit

---
+++ ArrayVersusDictionaryExample/ArrayVersusDictionaryExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrayVersusDictionaryExampleApp.swift
+//  ArrayVersusDictionaryExample
+//
+//  Created by Russell Gordon on 2025-05-21.
+//
+
+import SwiftUI
+
+@main
+struct ArrayVersusDictionaryExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
